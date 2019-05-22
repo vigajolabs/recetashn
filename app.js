e242a096ca5dc99ed8ba2efd362be2f42f6ee7cb
rlc <vigajolabs@gmail.com>
2019-05-22 16:03:20 -0600

    pruebas

--- app.js
+++ app.js
@@ -303,7 +303,7 @@ function handleDialogFlowAction(sender, action, messages, contexts, parameters) 
 		   let respuesta=JSON.parse(body);
 		   //console.log(respuesta);
 			  if (respuesta.hasOwnProperty("hits")){
-				    const imagen0=respuesta["hits"][0]["recipe"]["image"];
+				    const imagen0=respuesta["hits"]["recipe"]["image"];
 					console.log(imagen0);
 						
 					  }
