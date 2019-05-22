55163bbfac23ba5607219eab1074b85a93fb743f
rlc <vigajolabs@gmail.com>
2019-05-22 16:10:59 -0600

    extract

--- app.js
+++ app.js
@@ -303,7 +303,8 @@ function handleDialogFlowAction(sender, action, messages, contexts, parameters) 
 		   let respuesta=JSON.parse(body);
 		   //console.log(respuesta);
 			  if (respuesta.hasOwnProperty("hits")){
-				    const imagen0=respuesta["hits"][0]["recipe"]["image"][0];
+				    //const imagen0=respuesta["hits"][0]["recipe"]["image"];
+					const imagen0=respuesta.hits.[0].recipe.image;
 					console.log(imagen0);
 						
 					  }
