9226c8c020c0bb06c35d39f93f5b3d8f082263a8
rlc <vigajolabs@gmail.com>
2019-08-09 21:53:00 -0600

    prueba

--- app.js
+++ app.js
@@ -302,8 +302,8 @@ function handleDialogFlowAction(sender, action, messages, contexts, parameters) 
 		 if( response.statusCode === 200) {
 		   let respuesta=JSON.parse(body);
 		    console.log(respuesta);
-			 console.log(respuesta.hits);
-			   console.log(respuesta.hits.uri);
+			 //console.log(respuesta.hits);
+			   console.log(respuesta.hits.recipe);
 			  // if (respuesta.hasOwnProperty("hits")){ 	
 			// const imagen0=respuesta.hits;
 			 // console.log(imagen0);
